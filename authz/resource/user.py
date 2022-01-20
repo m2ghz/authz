74b70c4b16d43d67ec8fc6d5821f9e261b939056
Matthew <m2ghz@github.com>
2022-01-20 13:06:46 +0330

    adding and registering user resource

---
+++ authz/resource/user.py
@@ -0,0 +1,17 @@
+from flask_restful import Resource
+
+from authz.controller import UserController
+
+class UserResource(Resource):
+	
+	def get(self, user_id=None):
+		if user_id is None:
+			return UserController.get_users() # Get users list.
+		else:
+			return UserController.get_user(user_id) # Get single user.
+	def post(self):
+		return UserController.create_user() # Create new user.
+	def patch(self, user_id):
+		return UserController.update_user(user_id) # Update user
+	def delete(self, user_id):
+		return UserController.delete_user(user_id) # Delete user
